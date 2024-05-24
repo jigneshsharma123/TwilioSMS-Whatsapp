06efb96466740443114b11101db0b46c562a8b50
jigneshsharma123 <92665503+jigneshsharma123@users.noreply.github.com>
2024-05-24 12:51:49 +0530

    Remove secrets from the configuration

--- app/Config/Twilio.php
+++ app/Config/Twilio.php
@@ -6,8 +6,16 @@ use CodeIgniter\Config\BaseConfig;
 
 class Twilio extends BaseConfig
 {
-    public $account_sid = 'AC2bb17132f446785c31a93b645ee31caf';
-    public $auth_token = 'fdfe7062b2826ec6cd05f676e11a0211';
+    public $account_sid;
+    public $auth_token;
     public $twilio_number = '+14793515439';
     public $twilio_whatsapp_number = '+14155238886';
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->account_sid = getenv('TWILIO_ACCOUNT_SID');
+        $this->auth_token = getenv('TWILIO_AUTH_TOKEN');
+    }
 }
